59276f82d4f44c6869386021cc4ec18ff8682176
Ivan Barsukov <i.barsukov@protonmail.com>
2025-06-26 04:00:48 +0300

    Update translations

--- src/devtools/i18n/devtools_ru.ts
+++ src/devtools/i18n/devtools_ru.ts
@@ -4,57 +4,57 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../ui/about_dialog.py" line="113"/>
+        <location filename="../ui/about_dialog.py" line="136"/>
         <source> by NextGIS</source>
         <translation> от NextGIS</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="256"/>
+        <location filename="../ui/about_dialog.py" line="279"/>
         <source>REPORT_END</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="261"/>
+        <location filename="../ui/about_dialog.py" line="284"/>
         <source>Developers</source>
         <translation>Разработчики</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="262"/>
+        <location filename="../ui/about_dialog.py" line="285"/>
         <source>Homepage</source>
         <translation>Домашняя страница</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="264"/>
+        <location filename="../ui/about_dialog.py" line="287"/>
         <source>Please report bugs at</source>
         <translation>Пожалуйста, сообщайте об ошибках в</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="266"/>
+        <location filename="../ui/about_dialog.py" line="289"/>
         <source>bugtracker</source>
         <translation>багтрекер</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="267"/>
+        <location filename="../ui/about_dialog.py" line="290"/>
         <source>Video with an overview of the plugin</source>
         <translation>Видео с обзором плагина</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="268"/>
+        <location filename="../ui/about_dialog.py" line="291"/>
         <source>Other helpful services by NextGIS</source>
         <translation>Другие полезные сервисы от NextGIS</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="269"/>
+        <location filename="../ui/about_dialog.py" line="292"/>
         <source>Convenient up-to-date data extracts for any place in the world</source>
         <translation>Удобная выборка актуальных данных из любой точки мира</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="272"/>
+        <location filename="../ui/about_dialog.py" line="295"/>
         <source>Fully featured Web GIS service</source>
         <translation>Полнофункциональный Веб ГИС-сервис</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog.py" line="263"/>
+        <location filename="../ui/about_dialog.py" line="286"/>
         <source>User Guide</source>
         <translation>Руководство пользователя</translation>
     </message>
@@ -112,47 +112,322 @@ p, li { white-space: pre-wrap; }
     </message>
 </context>
 <context>
+    <name>DebugButton</name>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="74"/>
+        <source>&lt;b&gt;Status:&lt;/b&gt; </source>
+        <translation>&lt;b&gt;Статус:&lt;/b&gt; </translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="83"/>
+        <source>stopped</source>
+        <translation>остановлен</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="84"/>
+        <source>Start</source>
+        <translation>Запустить</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="91"/>
+        <source>running</source>
+        <translation>запущен</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="100"/>
+        <source>Stop</source>
+        <translation>Остановить</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="99"/>
+        <source>client connected</source>
+        <translation>клиент подключён</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="114"/>
+        <source>&lt;b&gt;Adapter:&lt;/b&gt; </source>
+        <translation>&lt;b&gt;Адаптер:&lt;/b&gt; </translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_button.py" line="125"/>
+        <source>Unable to start debugging</source>
+        <translation>Не удалось запустить отладку</translation>
+    </message>
+</context>
+<context>
+    <name>DebugManager</name>
+    <message>
+        <location filename="../debug/debug_manager.py" line="62"/>
+        <source>Debug already started</source>
+        <translation>Отладка уже запущена</translation>
+    </message>
+</context>
+<context>
+    <name>DebugSettingsErrorPage</name>
+    <message>
+        <location filename="../debug/ui/debug_settings_page.py" line="169"/>
+        <source>An error occurred while loading settings page</source>
+        <translation>Произошла ошибка при загрузке страницы настроек</translation>
+    </message>
+</context>
+<context>
+    <name>DebugSettingsPageBase</name>
+    <message>
+        <location filename="../debug/ui/debug_settings_page_base.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_settings_page_base.ui" line="20"/>
+        <source>General</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_settings_page_base.ui" line="28"/>
+        <source>Debug adapter</source>
+        <translation>Адаптер отладки</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_settings_page_base.ui" line="60"/>
+        <source>Enable debugger on QGIS startup</source>
+        <translation>Включить отладчик при запуске QGIS</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_settings_page_base.ui" line="67"/>
+        <source>Show notification when debugger starts</source>
+        <translation>Показывать уведомление при запуске отладчика</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/debug_settings_page_base.ui" line="77"/>
+        <source>Adapter settings</source>
+        <translation>Настройки адаптера</translation>
+    </message>
+</context>
+<context>
+    <name>DebugSettingsPageFactory</name>
+    <message>
+        <location filename="../debug/ui/debug_settings_page.py" line="200"/>
+        <source>Debug</source>
+        <translation>Отладка</translation>
+    </message>
+</context>
+<context>
+    <name>DebugpyAdapter</name>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="122"/>
+        <source>&quot;{lib_name}&quot; library is not installed.</source>
+        <translation>Библиотека &quot;{lib_name}&quot; не установлена.</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="259"/>
+        <source>Client connected</source>
+        <translation>Клиент подключён</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="264"/>
+        <source>Client disconnected</source>
+        <translation>Клиент отключён</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="271"/>
+        <source>Copy launch.json template</source>
+        <translation>Скопировать шаблон launch.json</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="271"/>
+        <source>Copied!</source>
+        <translation>Скопировано!</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="277"/>
+        <source>Debug session started at {self.__active_hostname}:{self.__active_port}</source>
+        <translation>Сессия отладки запущена на {self.__active_hostname}:{self.__active_port}</translation>
+    </message>
+</context>
+<context>
+    <name>DebugpySettingsPageBase</name>
+    <message>
+        <location filename="../debug/adapters/debugpy/ui/debugpy_settings_page_base.ui" line="14"/>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/ui/debugpy_settings_page_base.ui" line="32"/>
+        <source>Hostname</source>
+        <translation>Имя хоста</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/ui/debugpy_settings_page_base.ui" line="39"/>
+        <source>Port</source>
+        <translation>Порт</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/ui/debugpy_settings_page_base.ui" line="51"/>
+        <source>Auto select</source>
+        <translation>Автовыбор</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/ui/debugpy_settings_page_base.ui" line="82"/>
+        <source>–</source>
+        <translation>–</translation>
+    </message>
+</context>
+<context>
     <name>DevToolsPlugin</name>
     <message>
-        <location filename="../devtools_plugin.py" line="121"/>
+        <location filename="../devtools_plugin.py" line="157"/>
         <source>About plugin…</source>
         <translation>О модуле…</translation>
+    </message>
+    <message>
+        <location filename="../devtools_plugin.py" line="205"/>
+        <source>Settings…</source>
+        <translation>Настройки…</translation>
+    </message>
+</context>
+<context>
+    <name>DevToolsSettingsErrorPage</name>
+    <message>
+        <location filename="../ui/devtools_settings_page.py" line="120"/>
+        <source>An error occurred while loading settings page</source>
+        <translation>Произошла ошибка при загрузке страницы настроек</translation>
+    </message>
+</context>
+<context>
+    <name>DevToolsSettingsPageBase</name>
+    <message>
+        <location filename="../ui/devtools_settings_page_base.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <location filename="../ui/devtools_settings_page_base.ui" line="20"/>
+        <source>Other</source>
+        <translation>Другое</translation>
+    </message>
+    <message>
+        <location filename="../ui/devtools_settings_page_base.ui" line="26"/>
+        <source>Enable plugin debug messages</source>
+        <translation>Включить отладочные сообщения плагина</translation>
     </message>
 </context>
 <context>
     <name>Exceptions</name>
     <message>
-        <location filename="../core/exceptions.py" line="42"/>
+        <location filename="../debug/exceptions.py" line="58"/>
         <source>An error occurred while running the plugin</source>
         <translation>При работе плагина возникла ошибка</translation>
     </message>
     <message>
-        <location filename="../core/exceptions.py" line="149"/>
+        <location filename="../core/exceptions.py" line="239"/>
         <source>The plugin has been successfully updated. To continue working, please restart QGIS.</source>
         <translation>Плагин был успешно обновлён. Чтобы продолжить работу, пожалуйста, перезапустите QGIS.</translation>
+    </message>
+    <message>
+        <location filename="../core/exceptions.py" line="277"/>
+        <source>Failed to load the user interface.</source>
+        <translation>Не удалось загрузить пользовательский интерфейс.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="82"/>
+        <source>&quot;{lib_name}&quot; library is not installed.</source>
+        <translation>Библиотека &quot;{lib_name}&quot; не установлена.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="85"/>
+        <source>Please install the required package.</source>
+        <translation>Пожалуйста, установите необходимый пакет.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="103"/>
+        <source>Port {port} is already in use.</source>
+        <translation>Порт {port} уже используется.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="106"/>
+        <source>Please choose another port in settings.</source>
+        <translation>Пожалуйста, выберите другой порт в настройках.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="113"/>
+        <source>Open settings</source>
+        <translation>Открыть настройки</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="126"/>
+        <source>Debug session is already running.</source>
+        <translation>Сессия отладки уже запущена.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="141"/>
+        <source>Debug session has already been started in this process.</source>
+        <translation>Сессия отладки уже была запущена в этом процессе.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="145"/>
+        <source>Please restart QGIS.</source>
+        <translation>Пожалуйста, перезапустите QGIS.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="149"/>
+        <source>Multiple starts can cause the debuggee to hang.</source>
+        <translation>Многократный запуск может привести к зависанию отлаживаемого процесса.</translation>
     </message>
 </context>
 <context>
     <name>MessageBarNotifier</name>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="159"/>
+        <location filename="../notifier/message_bar_notifier.py" line="179"/>
         <source>Try again</source>
         <translation>Повторить</translation>
     </message>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="164"/>
+        <location filename="../notifier/message_bar_notifier.py" line="189"/>
         <source>Details</source>
         <translation>Подробнее</translation>
     </message>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="168"/>
+        <location filename="../notifier/message_bar_notifier.py" line="193"/>
         <source>Open logs</source>
         <translation>Открыть логи</translation>
     </message>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="173"/>
+        <location filename="../notifier/message_bar_notifier.py" line="198"/>
         <source>Let us know</source>
         <translation>Сообщить об ошибке</translation>
+    </message>
+</context>
+<context>
+    <name>StatusWidgetBase</name>
+    <message>
+        <location filename="../debug/ui/status_widget_base.ui" line="20"/>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/status_widget_base.ui" line="43"/>
+        <source>&lt;h3&gt;Debug&lt;/h3&gt;</source>
+        <translation>&lt;h3&gt;Отладка&lt;/h3&gt;</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/status_widget_base.ui" line="50"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/status_widget_base.ui" line="69"/>
+        <source>&lt;b&gt;Adapter:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Адаптер:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/status_widget_base.ui" line="82"/>
+        <source>&lt;b&gt;Status:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Статус:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../debug/ui/status_widget_base.ui" line="97"/>
+        <source>Start</source>
+        <translation>Запустить</translation>
     </message>
 </context>
 </TS>
