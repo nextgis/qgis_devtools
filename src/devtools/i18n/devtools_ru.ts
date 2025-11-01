6f89a10236c0c90465dc5fb7395973486767ff45
Ivan Barsukov <i.barsukov@protonmail.com>
2025-11-01 21:44:41 +0300

    Update translations

--- src/devtools/i18n/devtools_ru.ts
+++ src/devtools/i18n/devtools_ru.ts
@@ -114,42 +114,42 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>DebugButton</name>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="76"/>
+        <location filename="../debug/ui/debug_button.py" line="77"/>
         <source>&lt;b&gt;Status:&lt;/b&gt; </source>
         <translation>&lt;b&gt;Статус:&lt;/b&gt; </translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="85"/>
+        <location filename="../debug/ui/debug_button.py" line="86"/>
         <source>stopped</source>
         <translation>остановлен</translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="86"/>
+        <location filename="../debug/ui/debug_button.py" line="87"/>
         <source>Start</source>
         <translation>Запустить</translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="93"/>
+        <location filename="../debug/ui/debug_button.py" line="94"/>
         <source>running</source>
         <translation>запущен</translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="102"/>
+        <location filename="../debug/ui/debug_button.py" line="103"/>
         <source>Stop</source>
         <translation>Остановить</translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="101"/>
+        <location filename="../debug/ui/debug_button.py" line="102"/>
         <source>client connected</source>
         <translation>клиент подключён</translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="116"/>
+        <location filename="../debug/ui/debug_button.py" line="117"/>
         <source>&lt;b&gt;Adapter:&lt;/b&gt; </source>
         <translation>&lt;b&gt;Адаптер:&lt;/b&gt; </translation>
     </message>
     <message>
-        <location filename="../debug/ui/debug_button.py" line="127"/>
+        <location filename="../debug/ui/debug_button.py" line="128"/>
         <source>Unable to start debugging</source>
         <translation>Не удалось запустить отладку</translation>
     </message>
@@ -157,9 +157,24 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>DebugManager</name>
     <message>
-        <location filename="../debug/debug_manager.py" line="85"/>
+        <location filename="../debug/debug_manager.py" line="97"/>
         <source>Debug already started</source>
         <translation>Отладка уже запущена</translation>
+    </message>
+    <message>
+        <location filename="../debug/debug_manager.py" line="162"/>
+        <source>Debug script</source>
+        <translation>Запустить отладку сценария</translation>
+    </message>
+    <message>
+        <location filename="../debug/debug_manager.py" line="307"/>
+        <source>Unsaved script</source>
+        <translation>Несохранённый сценарий</translation>
+    </message>
+    <message>
+        <location filename="../debug/debug_manager.py" line="308"/>
+        <source>Breakpoints will not work with unsaved files. Do you want to continue debugging the unsaved script?</source>
+        <translation>Точки останова не будут работать с несохранёнными файлами. Продолжить отладку несохранённого сценария?</translation>
     </message>
 </context>
 <context>
@@ -214,27 +229,22 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>DebugpyAdapter</name>
     <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="124"/>
-        <source>&quot;{lib_name}&quot; library is not installed.</source>
-        <translation>Библиотека &quot;{lib_name}&quot; не установлена.</translation>
-    </message>
-    <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="280"/>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="371"/>
         <source>Client connected</source>
         <translation>Клиент подключён</translation>
     </message>
     <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="285"/>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="376"/>
         <source>Client disconnected</source>
         <translation>Клиент отключён</translation>
     </message>
     <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="292"/>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="383"/>
         <source>Copy launch.json template</source>
         <translation>Скопировать шаблон launch.json</translation>
     </message>
     <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="292"/>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="383"/>
         <source>Copied!</source>
         <translation>Скопировано!</translation>
     </message>
@@ -244,14 +254,34 @@ p, li { white-space: pre-wrap; }
         <translation type="obsolete">Сессия отладки запущена на {self.__active_hostname}:{self.__active_port}</translation>
     </message>
     <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="128"/>
-        <source>Installation instructions can be found in the user guide.</source>
-        <translation>Инструкции по установке можно найти в руководстве пользователя.</translation>
-    </message>
-    <message>
-        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="298"/>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="399"/>
         <source>Debug session started at {hostname}:{port}</source>
         <translation>Сессия отладки запущена на {hostname}:{port}</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="256"/>
+        <source>Cannot start debugging</source>
+        <translation>Невозможно начать отладку</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="266"/>
+        <source>User Guide</source>
+        <translation>Руководство пользователя</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="274"/>
+        <source>Waiting for client...</source>
+        <translation>Ожидание клиента...</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="275"/>
+        <source>Waiting for client to connect to debugger at {host}:{port}</source>
+        <translation>Ожидание подключения клиента к отладчику по адресу {host}:{port}</translation>
+    </message>
+    <message>
+        <location filename="../debug/adapters/debugpy/debugpy_adapter.py" line="387"/>
+        <source>Copy launch.json template with path mappings</source>
+        <translation>Скопировать шаблон launch.json с указанием путей</translation>
     </message>
 </context>
 <context>
@@ -285,37 +315,37 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>DevToolsPlugin</name>
     <message>
-        <location filename="../devtools_plugin.py" line="161"/>
+        <location filename="../devtools_plugin.py" line="181"/>
         <source>About plugin…</source>
         <translation>О модуле…</translation>
     </message>
     <message>
-        <location filename="../devtools_plugin.py" line="209"/>
+        <location filename="../devtools_plugin.py" line="229"/>
         <source>Settings…</source>
         <translation>Настройки…</translation>
     </message>
     <message>
-        <location filename="../devtools_plugin.py" line="258"/>
+        <location filename="../devtools_plugin.py" line="321"/>
         <source>Open Changelog</source>
         <translation>Открыть список изменений</translation>
     </message>
     <message>
-        <location filename="../devtools_plugin.py" line="261"/>
+        <location filename="../devtools_plugin.py" line="324"/>
         <source>Open User Guide</source>
         <translation>Открыть руководство пользователя</translation>
     </message>
     <message>
-        <location filename="../devtools_plugin.py" line="264"/>
+        <location filename="../devtools_plugin.py" line="327"/>
         <source>About Plugin…</source>
         <translation>О модуле…</translation>
     </message>
     <message>
-        <location filename="../devtools_plugin.py" line="268"/>
+        <location filename="../devtools_plugin.py" line="331"/>
         <source>Plugin was successfully installed</source>
         <translation>Плагин был успешно установлен</translation>
     </message>
     <message>
-        <location filename="../devtools_plugin.py" line="271"/>
+        <location filename="../devtools_plugin.py" line="334"/>
         <source>Plugin was successfully updated</source>
         <translation>Плагин был успешно обновлён</translation>
     </message>
@@ -369,70 +399,70 @@ p, li { white-space: pre-wrap; }
         <translation>Библиотека &quot;{lib_name}&quot; не установлена.</translation>
     </message>
     <message>
-        <location filename="../debug/exceptions.py" line="85"/>
-        <source>Please install the required package.</source>
-        <translation>Пожалуйста, установите необходимый пакет.</translation>
-    </message>
-    <message>
-        <location filename="../debug/exceptions.py" line="103"/>
+        <location filename="../debug/exceptions.py" line="111"/>
         <source>Port {port} is already in use.</source>
         <translation>Порт {port} уже используется.</translation>
     </message>
     <message>
-        <location filename="../debug/exceptions.py" line="106"/>
+        <location filename="../debug/exceptions.py" line="114"/>
         <source>Please choose another port in settings.</source>
         <translation>Пожалуйста, выберите другой порт в настройках.</translation>
     </message>
     <message>
-        <location filename="../debug/exceptions.py" line="113"/>
+        <location filename="../debug/exceptions.py" line="122"/>
         <source>Open settings</source>
         <translation>Открыть настройки</translation>
     </message>
     <message>
-        <location filename="../debug/exceptions.py" line="126"/>
+        <location filename="../debug/exceptions.py" line="150"/>
         <source>Debug session is already running.</source>
         <translation>Сессия отладки уже запущена.</translation>
     </message>
     <message>
-        <location filename="../debug/exceptions.py" line="141"/>
+        <location filename="../debug/exceptions.py" line="165"/>
         <source>Debug session has already been started in this process.</source>
         <translation>Сессия отладки уже была запущена в этом процессе.</translation>
     </message>
     <message>
-        <location filename="../debug/exceptions.py" line="145"/>
-        <source>Please restart QGIS.</source>
-        <translation>Пожалуйста, перезапустите QGIS.</translation>
-    </message>
-    <message>
-        <location filename="../debug/exceptions.py" line="149"/>
-        <source>Multiple starts can cause the debuggee to hang. </source>
-        <translation>Многократный запуск может привести к зависанию отлаживаемого процесса. </translation>
-    </message>
-    <message>
-        <location filename="../debug/exceptions.py" line="149"/>
+        <location filename="../debug/exceptions.py" line="173"/>
         <source>This is a limitation of the used debug library.</source>
         <translation>Это ограничение используемой библиотеки отладки.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="169"/>
+        <source>&lt;b&gt;Please restart QGIS&lt;/b&gt;.</source>
+        <translation>&lt;b&gt;Пожалуйста, перезагрузите QGIS&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="173"/>
+        <source>Multiple debug starts can cause the debuggee to hang.</source>
+        <translation>Многократный запуск может привести к зависанию отлаживаемого процесса.</translation>
+    </message>
+    <message>
+        <location filename="../debug/exceptions.py" line="86"/>
+        <source>Installation instructions can be found in the user guide.</source>
+        <translation>Инструкции по установке можно найти в руководстве пользователя.</translation>
     </message>
 </context>
 <context>
     <name>MessageBarNotifier</name>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="179"/>
+        <location filename="../notifier/message_bar_notifier.py" line="183"/>
         <source>Try again</source>
         <translation>Повторить</translation>
     </message>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="189"/>
+        <location filename="../notifier/message_bar_notifier.py" line="193"/>
         <source>Details</source>
         <translation>Подробнее</translation>
     </message>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="193"/>
+        <location filename="../notifier/message_bar_notifier.py" line="197"/>
         <source>Open logs</source>
         <translation>Открыть логи</translation>
     </message>
     <message>
-        <location filename="../notifier/message_bar_notifier.py" line="198"/>
+        <location filename="../notifier/message_bar_notifier.py" line="202"/>
         <source>Let us know</source>
         <translation>Сообщить об ошибке</translation>
     </message>
@@ -473,6 +503,14 @@ p, li { white-space: pre-wrap; }
         <location filename="../debug/ui/status_widget_base.ui" line="56"/>
         <source>User Guide</source>
         <translation>Руководство пользователя</translation>
+    </message>
+</context>
+<context>
+    <name>WaitingDialogBase</name>
+    <message>
+        <location filename="../shared/ui/waiting_dialog_base.ui" line="20"/>
+        <source>waiting...</source>
+        <translation>ожидание...</translation>
     </message>
 </context>
 </TS>
